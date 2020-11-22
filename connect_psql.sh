10f362e21eaaf93ca6ca23a3599d750f5f804851
ValliereMagic <louisemelaney@outlook.com>
2020-11-22 16:14:32 -0500

    Set up database with an initialization script; write a script for placing secrets where they need to go.

--- connect_psql.sh
+++ connect_psql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Connect to the database container from the nplbam container
-sudo docker exec -it nplbam bash -c "PGPASSWORD=\"$(cat postgres_password.enc)\" psql -h nplbam_db -U postgres"
+sudo docker exec -it nplbam bash -c "PGPASSWORD=\"$(cat postgres_root_password.enc)\" psql -h nplbam_db -U postgres"
--- start_postgres.sh
+++ start_postgres.sh
@@ -3,5 +3,5 @@
 sudo docker run -d --rm --name nplbam_db \
     --network=nplbam-net \
     -v "$(pwd)/postgres":/var/lib/postgresql/data \
-    -e POSTGRES_PASSWORD="$(cat postgres_password.enc)" \
-    postgres:13
+    -e POSTGRES_PASSWORD="$(cat postgres_root_password.enc)" \
+    nplbam_postgres
