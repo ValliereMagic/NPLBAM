e6671c91bf48005e963c63569a97b3177c10e902
nevas <gillbertz@gmail.com>
2020-11-29 20:29:30 -0500

    Changes to be committed: 	modified:   nplbam/app/__init__.py - Changed newAnimal to new_animal - Redirect to /animals if already logged in 	modified:   nplbam/app/db/db.py - creates database when run now 	renamed:    nplbam/app/newAnimal.py -> nplbam/app/new_animal.py 	renamed:    nplbam/app/templates/addDog.html -> nplbam/app/templates/add_dog.html 	modified:   nplbam/app/templates/animals.html - Changed newAnimal/addDog to be named new_animal and add_dog

--- nplbam/app/new_animal.py
+++ nplbam/app/new_animal.py
@@ -1,9 +1,9 @@
 from flask import render_template, Blueprint, session as flask_session, redirect
 
-bp = Blueprint('newAnimal', __name__, url_prefix="")
+bp = Blueprint('new_animal', __name__, url_prefix="")
 
-@bp.route("/newAnimal")
+@bp.route("/new_animal")
 def query():
     if flask_session.get("userID", default=None) is None:
         return redirect("/")
-    return render_template("addDog.html", title="Add Dog")
+    return render_template("add_dog.html", title="Add Dog")
