204af1b5322ee0978463b894954a01e34f991966
nevas <gillbertz@gmail.com>
2020-12-21 21:42:16 -0500

    Changes to be committed: 	new file:   nplbam/app/jsons/dog_questions.txt 	- Added a json of all the questions related to dogs 	modified:   nplbam/app/new_animal.py         - Changed to get json of dog questions         - Changed to new template for all animals, not just dog         - Need to still hook up to database 	new file:   nplbam/app/templates/add_animal.html         - New Template which replaces Add_Dog         - Uses Jinga2 templating to dynamically change based on animal         - Still needs to be hooked up for submitting files/pictures 	deleted:    nplbam/app/templates/add_dog.html         - See Above 	modified:   nplbam/app/templates/animals.html         - Now uses Jinja2 Templating to show the navigation bar 	new file:   nplbam/app/templates/base_template.html         - New Base templating from which all but index will inherit from.         - Has the nav bar and head. Still needs to do roles 	deleted:    nplbam/app/templates/header.html         - Useless, replaced by Above 	modified:   start_flask.sh         - Can now start interactive mode using -i. YaaaaaaY

--- nplbam/app/new_animal.py
+++ nplbam/app/new_animal.py
@@ -1,10 +1,24 @@
 from flask import render_template, Blueprint, session as flask_session, redirect
-
+import json
 bp = Blueprint('new_animal', __name__, url_prefix="")
 
 
+# Route for the new animal page.
 @bp.route("/new_animal")
 def new_animal():
+    # Check if they are logged in
     if flask_session.get("userID", default=None) is None:
         return redirect("/")
-    return render_template("add_dog.html", title="Add Dog")
+    # Open the JSON with the questions for dog
+    with open('nplbam/app/jsons/dog_questions.txt') as json_file:
+        questions = json.load(json_file)
+    # Create the form page dynamically using the add_animal template and the questions
+    return render_template("add_animal.html", questions=questions,  title="Add Dog")
+
+# Route for getting the data from the form to put in the database
+@bp.route("/animal_added", methods=['GET', 'POST'])
+def animal_added():
+    return redirect("/animals")
+    # Need to Add Data to database.
+ 
+    
