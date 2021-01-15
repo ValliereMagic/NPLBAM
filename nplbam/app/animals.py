9a01909c23d0633b7b10f294df7f1a5d5ff85250
ValliereMagic <louisemelaney@outlook.com>
2021-01-14 20:44:50 -0500

    Requested formatting and docstrings.

--- nplbam/app/animals.py
+++ nplbam/app/animals.py
@@ -5,6 +5,9 @@ bp = Blueprint('animals', __name__, url_prefix="")
 
 @bp.route("/animals")
 def query():
+    """
+    Page with the list of all animals, which can be filtered.
+    """
     if flask_session.get("userID", default=None) is None:
         return redirect("/")
     return render_template("animals.html", title="Animals")
