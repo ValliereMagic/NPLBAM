334face0af5dbfb9763422cc9a8fa0b8c6617b99
nevas <gillbertz@gmail.com>
2021-01-21 14:17:45 -0500

    Changes to be committed: 	modified:   nplbam/app/__init__.py         - New blueprints added (accounts and view_animal)
    	modified:   nplbam/app/new_animal.py
    	- Now correctly inserts to the StageInfo table too
    
    	modified:   nplbam/app/animals.py
            - Modified to get calculate the amount of days each animal has been in the stage
    
    	modified:   nplbam/app/templates/animals.html
    	- Modified to allow the days in stage column
            - Now hrefs to the new view_animal page (temporary)
    
    	new file:   nplbam/app/templates/view_animal.html
    	- New page that displays animal information from the form
    	- Basically is the same as the form but with values added and locked from input
    
    	new file:   nplbam/app/view_animal.py
    	- Routes and puts the information into the view_animal page
    
    	new file:   nplbam/app/templates/accounts.html
    	- New page that displays all the accounts in the system.
            - Basically a reskinned version of animals.html
    
    	new file:   nplbam/app/accounts.py
    	- Routes and gets information for the accounts.html

--- nplbam/app/accounts.py
+++ nplbam/app/accounts.py
@@ -1,16 +1,16 @@
 from flask import Blueprint, redirect, render_template
 from flask import session as flask_session
-from datetime import date
+
 from sqlalchemy.orm import sessionmaker, relationship
 from .db import db
 
-bp = Blueprint('animals', __name__, url_prefix="")
+bp = Blueprint('accounts', __name__, url_prefix="")
 
 
-@bp.route("/animals")
+@bp.route("/accounts")
 def query():
     """
-    Page with the list of all animals, which can be filtered.
+    Page with the list of all accounts, which can be filtered.
     """
     # Make sure visitor is logged in
     if flask_session.get("userID", default=None) is None:
@@ -18,9 +18,6 @@ def query():
     # Get the list of animals from the database
     engine = db.get_db_engine()
     db_session = (sessionmaker(bind=engine))()
-    animals_list = db_session.query(db.Animals).all()
+    accounts_list = db_session.query(db.Users).all()
     db_session.close()
-    # Calculate the total amount of days each animal has been in that stage
-    for animal in animals_list:
-        animal.days = (date.today() - animal.stageDate).days 
-    return render_template("animals.html", title="Animals", animals=animals_list)
+    return render_template("accounts.html", title="Accounts", accounts=accounts_list)
