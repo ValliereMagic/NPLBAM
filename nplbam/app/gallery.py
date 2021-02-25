7c7061ff5990da9891acfc752b432ca98cf7b37e
Devon Ladd <devonaladd@gmail.com>
2021-02-25 16:49:57 -0500

    Requested changes

--- nplbam/app/gallery.py
+++ nplbam/app/gallery.py
@@ -1,15 +1,11 @@
 import json
 from datetime import date
 
-from flask import Blueprint, redirect, render_template, request, current_app, url_for
+from flask import Blueprint, redirect, render_template, request
 from flask import session as flask_session
 from sqlalchemy.orm import sessionmaker
 
 from .db import db
-
-import os
-
-import nplbam.app.file_downloads as file_downloads
 
 bp = Blueprint('gallery', __name__, url_prefix="")
 
@@ -56,24 +52,3 @@ def gallery():
 
     # Render the page
     return render_template("gallery.html", animalID=viewID, title="View {}".format(animal_entry.name), info=info, images=images)
-
-# Route to view animal page.
-@bp.route("/animal_viewed", methods=['POST', 'GET'])
-def animal_viewed():
-    """
-    This is a subpage that is used to get the button click from the animal view page.
-    Can redirect back to the calling page or to the edit animal page depending on which
-    button was clicked.
-    """
-    # Make sure visitor is logged in
-    if flask_session.get("userID", default=None) is None:
-        return redirect("/")
-    # Make sure they got here with post
-    if request.method == 'POST':
-        if 'edit' in request.form:
-            id: str = request.form['animalId']
-            redir = "/edit_animal?editid={}".format(id)
-            return redirect(redir)
-        else:
-            redirect("/accounts")
-    return redirect("/animals")
