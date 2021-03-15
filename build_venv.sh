4fcd72b310b141dcb43b743807862252d36c6201
Trevor Gilbert <46726401+Reavas@users.noreply.github.com>
2021-03-15 16:13:00 -0400

    Update build_venv.sh

--- build_venv.sh
+++ build_venv.sh
@@ -2,6 +2,6 @@
 # Create a local virtual python environment to do development work within
 # Install required libraries within the virtual environment.
 # This requires python3-pip and python3-venv on debian.
-python3 -m venv nplbamNVz
+python3 -m venv nplbamNV
 source nplbamNV/bin/activate
 pip install flask pynacl SQLAlchemy psycopg2-binary matplotlib
