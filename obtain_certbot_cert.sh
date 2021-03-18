050b72f03bc6bd53e276ae26e57b519d0fc6338c
ValliereMagic <louisemelaney@outlook.com>
2021-03-18 17:03:26 -0400

    Fix hiccups found while deploying on nplbam.strangled.net

--- obtain_certbot_cert.sh
+++ obtain_certbot_cert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker run --rm --name nplbam_certbot \
+sudo docker run -it --rm --name nplbam_certbot \
 	-v "$(pwd)/nginx/tls":/etc/letsencrypt \
 	-v "$(pwd)/nginx/tls":/var/lib/letsencrypt \
 	-v "$(pwd)/nginx/root":/webroot \
