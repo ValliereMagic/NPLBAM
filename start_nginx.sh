09a4f0d23401777b0195e8c099bb72444035c007
ValliereMagic <louisemelaney@outlook.com>
2021-03-18 17:05:33 -0400

    Add same ports in start_nginx just in case.

--- start_nginx.sh
+++ start_nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start the nginx reverse proxy and static content server
-sudo docker run -d --rm -p 127.0.0.1:8080:80 --name nplbam_rp \
+sudo docker run -d --rm -p 0.0.0.0:80:80 -p 0.0.0.0:443:443 --name nplbam_rp \
     -v "$(pwd)/nginx/nginx.conf":/etc/nginx/nginx.conf:ro \
     -v "$(pwd)/nginx/root":/usr/share/nginx/html:ro \
     -v "$(pwd)/nginx/tls":/etc/nginx/tls:ro \
