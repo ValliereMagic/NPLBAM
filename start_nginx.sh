4ca4fdc9909bc434dcd30b3f14580d3f943fd596
ValliereMagic <ValliereMagic@users.noreply.github.com>
2021-03-18 16:23:36 -0400

    Create tools to setup TLS encryption.

--- start_nginx.sh
+++ start_nginx.sh
@@ -3,5 +3,6 @@
 sudo docker run -d --rm -p 127.0.0.1:8080:80 --name nplbam_rp \
     -v "$(pwd)/nginx/nginx.conf":/etc/nginx/nginx.conf:ro \
     -v "$(pwd)/nginx/root":/usr/share/nginx/html:ro \
+    -v "$(pwd)/nginx/tls":/etc/nginx/tls:ro \
     --network=nplbam-net \
     nginx
