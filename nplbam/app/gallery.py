1cbe5d64ccc272fe1054c0624496f46c6da4484e
nevas <gillbertz@gmail.com>
2021-04-16 14:48:11 -0400

    - Made Gallery much more robust

--- nplbam/app/gallery.py
+++ nplbam/app/gallery.py
@@ -44,6 +44,11 @@ def gallery():
     animal_entry = db_session.query(
         db.Animals).filter_by(animalID=viewID).first()
 
+    # Check if Animal Entry exists
+    if (animal_entry == None):
+        flash("Animal ID does not exist")
+        return redirect("/animals")
+
     # Get a dictionary for required information
     info = {}
 
